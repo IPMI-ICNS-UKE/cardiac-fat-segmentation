d25cea7a8f1a4c638d093922010eaead41c77efc
GeorgBell <georgbell5@gmail.com>
2026-09-11 15:08:10 +0200

    Set the train epochs to 100

--- nnUNet_baseline/nnUNet_scripts/03_train_2d.sh
+++ nnUNet_baseline/nnUNet_scripts/03_train_2d.sh
@@ -3,4 +3,8 @@ export nnUNet_raw="/home/gkolokolnikov/PhD_project/Project_EAT/cardiac-fat-segme
 export nnUNet_preprocessed="/home/gkolokolnikov/PhD_project/Project_EAT/cardiac-fat-segmentation/nnUNet_baseline/nnUNet_preprocessed"
 export nnUNet_results="/home/gkolokolnikov/PhD_project/Project_EAT/cardiac-fat-segmentation/nnUNet_baseline/nnUNet_results"
 
-nnUNetv2_train 001 2d 0
+nnUNetv2_train 001 2d 0 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 2d 1 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 2d 2 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 2d 3 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 2d 4 -tr nnUNetTrainer_100epochs
--- nnUNet_baseline/nnUNet_scripts/03_train_3d.sh
+++ nnUNet_baseline/nnUNet_scripts/03_train_3d.sh
@@ -3,4 +3,8 @@ export nnUNet_raw="/home/gkolokolnikov/PhD_project/Project_EAT/cardiac-fat-segme
 export nnUNet_preprocessed="/home/gkolokolnikov/PhD_project/Project_EAT/cardiac-fat-segmentation/nnUNet_baseline/nnUNet_preprocessed"
 export nnUNet_results="/home/gkolokolnikov/PhD_project/Project_EAT/cardiac-fat-segmentation/nnUNet_baseline/nnUNet_results"
 
-nnUNetv2_train 001 3d_fullres 0
+nnUNetv2_train 001 3d_fullres 0 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 3d_fullres 1 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 3d_fullres 2 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 3d_fullres 3 -tr nnUNetTrainer_100epochs
+nnUNetv2_train 001 3d_fullres 4 -tr nnUNetTrainer_100epochs
